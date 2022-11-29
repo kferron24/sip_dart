af56e117fa6ac8b6370a50c5259595525b65cdb6
Ezzarzari <aezzarzari@enseirb-matmeca.fr>
2022-11-29 10:26:32 +0100

    first version to be updated

--- android/app/src/main/kotlin/com/example/sip_dart/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sip_dart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sip_dart/main.dart';
+import 'package:dart_sip_ua_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
